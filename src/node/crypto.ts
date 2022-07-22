9c9bdd55ad9c6b0b18f5b5b372bbd80a4cae8684
renxia <lzwy0820@qq.com>
2022-07-22 18:06:41 +0800

    chore: update deps version

--- src/node/crypto.ts
+++ src/node/crypto.ts
@@ -1,21 +1,23 @@
 import { createHash } from 'crypto';
 import { existsSync, readFileSync } from 'fs';
+import { basename } from 'path';
 
 /**
  * 生成指定字符串或指定文件路径的md5值
- * @param str {string|Buffer} 指定的字符串，或者指定的文件路径
+ * @param str {string} 指定的字符串，或者指定的文件路径
  * @param isFile {boolean} str 是否为一个文件路径
  */
 export function md5(str: string | Buffer, isFile = false) {
   try {
-    if (isFile && typeof str === 'string') {
+    if (isFile) {
       if (!existsSync(str)) return '';
       str = readFileSync(str);
     }
-    return createHash('md5').update(str).digest('hex');
+    const md5 = createHash('md5').update(str).digest('hex');
+    return md5;
   } catch (error) {
     /* eslint-disable no-console */
-    console.warn('[md5][error]', (error as Error).message);
+    console.log(error);
     return '';
   }
 }
@@ -27,12 +29,55 @@ export function md5(str: string | Buffer, isFile = false) {
  */
 export function getMd5ByPlainObject(params: Record<string | number, unknown>, filterKeyList: string[] = []) {
   if (!params) return '';
+  // const filterKeyList = ['current_page', 'page_size', 'uid', 'pageSize', 'currentPage'];
 
   const keys = Object.keys(params)
-    .filter(key => params[key] !== '' && !filterKeyList.includes(key))
-    .sort();
+    .sort()
+    .filter(key => params[key] !== '' && !filterKeyList.includes(key));
   if (keys.length === 0) return '';
 
   const data = keys.map(key => ({ key, value: params[key] }));
   return md5(JSON.stringify(data));
+}
+
+/**
+ * tar.gz 压缩。需添加依赖库 `compressing`
+ * @param srcDir 要压缩的目录路径
+ * @param dest 解压输出目录路径
+ */
+export async function tgzip(srcDir: string, dest?: string) {
+  if (!dest) dest = srcDir.replace(/\/$/, '');
+  if (!dest.endsWith('.tar.gz')) dest += '.tar.gz';
+
+  return import('compressing').then(({ tgz }) => tgz.compressDir(srcDir, dest));
+}
+
+/**
+ * tar.gz 文件解压。需添加依赖库 `compressing`
+ * @param tgzFilePath 要解压的 tar.gz 文件路径
+ * @param dest 解压输出目录路径
+ */
+export async function untgzip(tgzFilePath: string, dest?: string) {
+  return import('compressing').then(({ tgz }) => tgz.uncompress(tgzFilePath, dest || basename(tgzFilePath)));
+}
+
+/**
+ * zip 压缩。需添加依赖库 `compressing`
+ * @param srcDir 要压缩的目录路径
+ * @param dest 解压输出目录路径
+ */
+export async function zip(srcDir: string, dest?: string) {
+  if (!dest) dest = srcDir.replace(/\/$/, '');
+  if (!dest.endsWith('.zip')) dest += '.zip';
+
+  return import('compressing').then(({ zip }) => zip.compressDir(srcDir, dest));
+}
+
+/**
+ * zip 文件解压。需添加依赖库 `compressing`
+ * @param tgzFilePath 要解压的 tar.gz 文件路径
+ * @param dest 解压输出目录路径
+ */
+export async function unzip(tgzFilePath: string, dest?: string) {
+  return import('compressing').then(({ zip }) => zip.uncompress(tgzFilePath, dest || basename(tgzFilePath)));
 }
