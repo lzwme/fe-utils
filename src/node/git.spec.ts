c185b3008f00c56a4ce45de1b1552d4051b83c30
renxia <lzwy0820@qq.com>
2022-11-22 15:47:46 +0800

    wip: add new method `getGitLog`

--- src/node/git.spec.ts
+++ src/node/git.spec.ts
@@ -1,4 +1,4 @@
-import { isGitRepo } from './git';
+import { getGitLog, isGitRepo } from './git';
 
 let exists = true;
 let execSyncResult = '';
@@ -11,7 +11,21 @@ jest.mock('node:fs', () => ({
   readFileSync: () => '',
 }));
 jest.mock('node:child_process', () => ({
-  execSync: () => {
+  execSync: (cmd: string) => {
+    if (cmd.includes('git log')) {
+      return [
+        `a5840084eae998ee4a33ad091d004b9f856c1140`,
+        `a584008 _-_ ea936e476429e71abae4d2b92b95331414116e15 _-_ ea936e4 _-_ 3013cd5`,
+        `3013cd5a74bbfbab88cc9a0d9adbed745aee80d8 _-_ 2022-11-20 18:56:29 +0800 _-_ 2022-11-20 18:56:29 +0800`,
+        `renxia _-_ no@lzw.me _-_ no@lzw.me`,
+        `chore: update dependencies`,
+        `2 days ago`,
+        `2 days ago`,
+      ]
+        .join(' _-_ ')
+        .trim();
+    }
+
     if (execSyncResult) throw new Error(execSyncResult);
     return '';
   },
@@ -28,5 +42,17 @@ describe('git.ts', () => {
 
     exists = false;
     expect(isGitRepo('abc')).toBeFalsy();
+  });
+
+  it('getGitLog', () => {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const is = (a: unknown, b: unknown) => expect(a).toBe(b);
+
+    const res = getGitLog(1, process.cwd());
+    is(res.length, 1);
+    is(res[0]!.s!.length > 0, true);
+
+    is(getGitLog().length, 1);
+    is(getGitLog(3).length, 1);
   });
 });
--- src/node/git.ts
+++ src/node/git.ts
@@ -2,13 +2,70 @@
  * @Author: lzw
  * @Date: 2021-04-23 10:44:32
  * @LastEditors: lzw
- * @LastEditTime: 2022-11-15 15:35:03
+ * @LastEditTime: 2022-11-22 15:21:15
  * @Description: gh u 相关的命令。主要为常用的快捷工具方法
  */
 
 import { resolve } from 'node:path';
 import { fs } from './fs-system';
 import { execSync } from './exec';
+
+/** getGitLog 返回项的格式 */
+interface GitLogItem {
+  /** hash 提交对象（commit）的完整哈希字串 */
+  H?: string;
+  /** abbrevHash 提交对象的简短哈希字串 */
+  h?: string;
+  /** treeHash 树对象（tree）的完整哈希字串 */
+  T?: string;
+  /** abbrevTreeHash 树对象的简短哈希字串 */
+  t?: string;
+  /** parentHashes 父对象（parent）的完整哈希字串 */
+  P?: string;
+  /** abbrevParentHashes 父对象的简短哈希字串 */
+  p?: string;
+  /** authorName 作者（author）的名字 */
+  an?: string;
+  /** authorEmail 作者的电子邮件地址 */
+  ae?: string;
+  /** authorDate 作者修订日期 */
+  ad?: string;
+  /** authorDateRel 作者修订日期，按多久以前的方式显示 */
+  ar?: string;
+  /** committerName 提交者(committer)的名字 */
+  cn?: string;
+  /** committerEmail 提交者的电子邮件地址 */
+  ce?: string;
+  /** committerDate 提交日期 */
+  cd?: string;
+  /** committerDateRel 提交日期，按多久以前的方式显示 */
+  cr?: string;
+  /** subject 提交说明 */
+  s?: string;
+}
+
+/**
+ * 获取近 N 条日志的详细信息
+ * @param num 指定获取日志的数量
+ */
+export function getGitLog(num = 1, cwd?: string) {
+  num = Math.max(1, +num || 1);
+  const prettyFormat = ['H', 'h', 'T', 't', 'p', 'P', 'cd', 'ad', 'an', 'ae', 'ce', 's', 'ar', 'cr'];
+  const cmd = `git log -${num} --pretty="tformat:%${prettyFormat.join(' _-_ %')}" --date=iso`;
+  const logResult = execSync(cmd, 'pipe', cwd);
+  if (logResult.stderr) console.error('[getGitLog][error]', logResult.stderr);
+  const list = logResult.stdout.trim().split('\n');
+  const result: GitLogItem[] = list.map(line => {
+    const valList = line.split(' _-_ ');
+    // eslint-disable-next-line unicorn/no-array-reduce
+    return prettyFormat.reduce((r: GitLogItem, key: string, idx: number) => {
+      r[key as never] = valList[idx] as never;
+      return r;
+    }, {});
+  });
+
+  return result;
+}
 
 /** 获取当前的本地分支名 */
 export function getHeadBranch(baseDirectory = process.cwd()) {
