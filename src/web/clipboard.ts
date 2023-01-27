cada1eb8554025c787a7523866a23529bfb3083e
renxia <lzwy0820@qq.com>
2023-01-27 18:54:41 +0800

    wip(web/clipboard): 新增浏览器中的文本复制方法

---
+++ src/web/clipboard.ts
@@ -0,0 +1,22 @@
+/** 复制内容到剪切板 */
+export function copyToClipboard(msg: unknown) {
+  function copy(e: ClipboardEvent) {
+    e.preventDefault();
+    if (e.clipboardData) e.clipboardData.setData('text/plain', typeof msg === 'string' ? msg : JSON.stringify(msg));
+  }
+
+  document.addEventListener('copy', copy);
+  document.execCommand('copy');
+  document.removeEventListener('copy', copy);
+}
+
+/** 复制内容到剪切板2 */
+export function copyToClipboard2(msg: unknown) {
+  const oInput = document.createElement('input');
+  oInput.style.display = 'none';
+  oInput.value = typeof msg === 'string' ? msg : JSON.stringify(msg);
+  document.body.appendChild(oInput);
+  oInput.select();
+  document.execCommand('copy');
+  oInput.remove();
+}
