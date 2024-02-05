34a3d28380d1498d1ca92f3d3de48dca48e4e782
renxia <lzwy0820@qq.com>
2024-02-05 16:04:21 +0800

    feat(TOTP): 新增 TOTP 类，用于 Node.js 下简易的验证 2FA

--- src/node/lib/index.ts
+++ src/node/lib/index.ts
@@ -2,6 +2,7 @@ export * from './download';
 export { httpLinkChecker } from './httpLinkChecker';
 export * from './NLogger';
 export * from './request';
+export * from './TOTP';
 export * from './WXWork';
 export { WorkerPool } from './worker-pool/WorkerPool';
 
