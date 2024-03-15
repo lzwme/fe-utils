12e1a4c18004412f5af077458f2364f99aa3ede2
renxia <lzwy0820@qq.com>
2024-03-15 21:21:10 +0800

    fix: 修复 color 导出导致的引用异常

--- src/node/lib/index.ts
+++ src/node/lib/index.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: renxia
+ * @Date: 2023-03-29 10:57:26
+ * @LastEditors: renxia
+ * @LastEditTime: 2024-03-15 21:17:48
+ * @Description:
+ */
 export * from './download';
 export { httpLinkChecker } from './httpLinkChecker';
 export * from './NLogger';
@@ -5,4 +12,5 @@ export * from './request';
 export * from './TOTP';
 export * from './WXWork';
 export { WorkerPool } from './worker-pool/WorkerPool';
-export { default as color } from 'console-log-colors';
+// @ts-ignore
+export * as color from 'console-log-colors';
